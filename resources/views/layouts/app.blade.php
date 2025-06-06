9919b7358d336ff6d56876984498cc060e117c1e
Naufal Akbar Nugroho <naufalakbar378@gmail.com>
2025-06-06 23:28:49 +0700

    upd: implement template layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,100 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}" />
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+    <!-- Library / Plugin Css Build -->
+    <link rel="stylesheet" href="{{ asset('assets/css/core/libs.min.css') }}" />
+
+    <!-- Hope Ui Design System Css -->
+    <link rel="stylesheet" href="{{ asset('assets/css/hope-ui.min.css') }}" />
+
+    <!-- Custom Css -->
+    <link rel="stylesheet" href="{{ asset('assets/css/custom.min.css') }}" />
+
+    <!-- Dark Css -->
+    <link rel="stylesheet" href="{{ asset('assets/css/dark.min.css') }}" />
+
+    <!-- Customizer Css -->
+    <link rel="stylesheet" href="{{ asset('assets/css/customizer.min.css') }}" />
+
+    <!-- RTL Css -->
+    <link rel="stylesheet" href="{{ asset('assets/css/rtl.min.css') }}" />
+
+    <!-- Flatpickr css -->
+    <link rel="stylesheet" href="{{ asset('assets/vendor/flatpickr/dist/flatpickr.min.css') }}" />
+
+    <!-- Scripts -->
+    @vite(['resources/js/app.js'])
+</head>
+
+<body class="font-sans antialiased">
+    <!-- loader Start -->
+    <div id="loading">
+        <div class="loader simple-loader">
+            <div class="loader-body"></div>
         </div>
-    </body>
+    </div>
+    <!-- loader END -->
+
+    @include('layouts.partials.sidebar')
+
+    <main class="main-content">
+        <div class="position-relative iq-banner">
+            @include('layouts.partials.navbar')
+            {{ $slot }}
+        </div>
+
+        @include('layouts.partials.footer')
+    </main>
+
+    <!-- Library Bundle Script -->
+    <script src="{{ asset('assets/js/core/libs.min.js') }}"></script>
+
+    <!-- External Library Bundle Script -->
+    <script src="{{ asset('assets/js/core/external.min.js') }}"></script>
+
+    <!-- Widgetchart Script -->
+    <script src="{{ asset('assets/js/charts/widgetcharts.js') }}"></script>
+
+    <!-- mapchart Script -->
+    <script src="{{ asset('assets/js/charts/vectore-chart.js') }}"></script>
+    <script src="{{ asset('assets/js/charts/dashboard.js') }}"></script>
+
+    <!-- fslightbox Script -->
+    <script src="{{ asset('assets/js/plugins/fslightbox.js') }}"></script>
+
+    <!-- Settings Script -->
+    <script src="{{ asset('assets/js/plugins/setting.js') }}"></script>
+
+    <!-- Slider-tab Script -->
+    <script src="{{ asset('assets/js/plugins/slider-tabs.js') }}"></script>
+
+    <!-- Form Wizard Script -->
+    <script src="{{ asset('assets/js/plugins/form-wizard.js') }}"></script>
+
+    <!-- AOS Animation Plugin-->
+
+    <!-- App Script -->
+    <script src="{{ asset('assets/js/hope-ui.js') }}" defer></script>
+
+    <!-- Flatpickr Script -->
+    <script src="{{ asset('assets/vendor/flatpickr/dist/flatpickr.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/flatpickr.js') }}" defer></script>
+
+    <script src="{{ asset('assets/js/plugins/prism.mini.js') }}"></script>
+</body>
+
 </html>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,30 +1,91 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
-            </div>
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}" />
+
+    <!-- Library / Plugin Css Build -->
+    <link rel="stylesheet" href="{{ asset('assets/css/core/libs.min.css') }}" />
+
+    <!-- Hope Ui Design System Css -->
+    <link rel="stylesheet" href="{{ asset('assets/css/hope-ui.min.css') }}" />
+
+    <!-- Custom Css -->
+    <link rel="stylesheet" href="{{ asset('assets/css/custom.min.css') }}" />
+
+    <!-- Dark Css -->
+    <link rel="stylesheet" href="{{ asset('assets/css/dark.min.css') }}" />
+
+    <!-- Customizer Css -->
+    <link rel="stylesheet" href="{{ asset('assets/css/customizer.min.css') }}" />
+
+    <!-- RTL Css -->
+    <link rel="stylesheet" href="{{ asset('assets/css/rtl.min.css') }}" />
+
+    <!-- Flatpickr css -->
+    <link rel="stylesheet" href="{{ asset('assets/vendor/flatpickr/dist/flatpickr.min.css') }}" />
+
+    <!-- Scripts -->
+    @vite(['resources/js/app.js'])
+</head>
+
+<body class=" " data-bs-spy="scroll" data-bs-target="#elements-section" data-bs-offset="0" tabindex="0">
+    <!-- loader Start -->
+    <div id="loading">
+        <div class="loader simple-loader">
+            <div class="loader-body"></div>
         </div>
-    </body>
+    </div>
+    <!-- loader END -->
+
+    {{ $slot }}
+
+    <!-- Library Bundle Script -->
+    <script src="{{ asset('assets/js/core/libs.min.js') }}"></script>
+
+    <!-- External Library Bundle Script -->
+    <script src="{{ asset('assets/js/core/external.min.js') }}"></script>
+
+    <!-- Widgetchart Script -->
+    <script src="{{ asset('assets/js/charts/widgetcharts.js') }}"></script>
+
+    <!-- mapchart Script -->
+    <script src="{{ asset('assets/js/charts/vectore-chart.js') }}"></script>
+    <script src="{{ asset('assets/js/charts/dashboard.js') }}"></script>
+
+    <!-- fslightbox Script -->
+    <script src="{{ asset('assets/js/plugins/fslightbox.js') }}"></script>
+
+    <!-- Settings Script -->
+    <script src="{{ asset('assets/js/plugins/setting.js') }}"></script>
+
+    <!-- Slider-tab Script -->
+    <script src="{{ asset('assets/js/plugins/slider-tabs.js') }}"></script>
+
+    <!-- Form Wizard Script -->
+    <script src="{{ asset('assets/js/plugins/form-wizard.js') }}"></script>
+
+    <!-- AOS Animation Plugin-->
+
+    <!-- App Script -->
+    <script src="{{ asset('assets/js/hope-ui.js') }}" defer></script>
+
+    <!-- Flatpickr Script -->
+    <script src="{{ asset('assets/vendor/flatpickr/dist/flatpickr.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/flatpickr.js') }}" defer></script>
+
+    <script src="{{ asset('assets/js/plugins/prism.mini.js') }}"></script>
+</body>
+
 </html>
