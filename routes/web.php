49a876a90197009b8d378a4a245ce60b1a225234
Naufal Akbar Nugroho <naufalakbar378@gmail.com>
2025-06-06 23:35:53 +0700

    upd: change root redirect route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('/login');
+    return redirect('/auth/login');
 });
 
 Route::get('/docs', function () {
