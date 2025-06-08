850a23c3508ab8b96dadba11cac45b2fccd6d07d
Naufal Akbar Nugroho <naufalakbar378@gmail.com>
2025-06-08 22:05:53 +0700

    upd: name root route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect('/auth/login');
-});
+})->name('root');
 
 Route::get('/docs', function () {
     return view('welcome');
