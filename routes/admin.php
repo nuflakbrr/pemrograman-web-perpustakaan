11fd9aad5cdfb3bd5a6f2baf8bd212da4316ee4b
Naufal Akbar Nugroho <naufalakbar378@gmail.com>
2025-06-08 22:57:19 +0700

    upd: add middleware auth

--- routes/admin.php
+++ routes/admin.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('admin')->group(function () {
+Route::middleware('auth')->prefix('admin')->group(function () {
     Route::get('', [DashboardController::class, 'index'])->name('dashboard');
 
     // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
