fa1c2c1c0690305a69540e409d1ed84e28bb81c8
andris112334 <andrisunmer@gmail.com>
2025-06-24 22:07:48 +0700

    upd: sidebar peminjaman

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -186,8 +186,8 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ Route::currentRouteName() == 'dashboard' ? 'active' : '' }}"
-                        href="{{ route('dashboard') }}">
+                    <a class="nav-link {{ Route::currentRouteName() == 'peminjaman.index' ? 'active' : '' }}"
+                        href="{{ route('peminjaman.index') }}">
                         <i class="icon">
                             <svg class="icon-20" width="20" viewBox="0 0 24 24" fill="none"
                                 xmlns="http://www.w3.org/2000/svg">
