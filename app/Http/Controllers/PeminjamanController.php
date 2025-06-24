217b2822a0d1eb9b9a73364f106b15552babdacd
andris112334 <andrisunmer@gmail.com>
2025-06-24 22:05:38 +0700

    add: controller peminjaman

---
+++ app/Http/Controllers/PeminjamanController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Peminjaman;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class PeminjamanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        return view('admin.peminjaman.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): View
+    {
+        return view('admin.peminjaman.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Peminjaman $peminjaman): View
+    {
+        return view('admin.peminjaman.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Peminjaman $peminjaman)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Peminjaman $peminjaman)
+    {
+        //
+    }
+}
